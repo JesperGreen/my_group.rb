925cac4ea7d8e64aadd9cd5605402d444934f67d
Jesper Green Stromblad <jesper@Jespers-MacBook-Pro.local>
2016-07-31 23:41:15 -0700

    initial commit

---
+++ my_group.rb
@@ -0,0 +1,5 @@
+my_group = Array.new
+person_1 = {name:"Jesper", age: 22, gender: "male"}
+person_2 = {name:"Jesper2", age: 33, gender: "male"}
+person_3 = {name:"Jesper3", age: 44, gender: "female"}
+my_group.push person_1, person_2, person_3
