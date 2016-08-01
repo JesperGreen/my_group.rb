8c4f9536373cf0255b8822ccb6a36a83b4d4f26a
Jesper Green Stromblad <jesper@Jespers-MacBook-Pro.local>
2016-08-01 00:46:39 -0700

    Assignment 2

--- my_group.rb
+++ my_group.rb
@@ -1,5 +1,11 @@
 my_group = Array.new
+
 person_1 = {name:"Jesper", age: 22, gender: "male"}
 person_2 = {name:"Jesper2", age: 33, gender: "male"}
 person_3 = {name:"Jesper3", age: 44, gender: "female"}
+
 my_group.push person_1, person_2, person_3
+
+my_group.each do |person|
+  puts "#{person[:name]} is a #{person[:age]} old #{person[:gender]}"
+end
